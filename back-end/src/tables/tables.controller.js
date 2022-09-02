ad742785fa66ba6ac8732dcd04ac35e26708eb47
Felipe Reyes <felipereyes3004@gmail.com>
2022-09-02 13:43:46 -0400

    backend for user story 4 done

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -1,17 +1,49 @@
 const service = require("./tables.service");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const resService = require("../reservations/reservations.service")
 
-async function list(req, res, next) {
-  const data = await service.list();
-
-  res.json({
-    data,
-  });
+async function list(req, res) {
+  const data = await service.list()
+  // console.log(data)
+  res.json ({
+    data
+  })
 }
 
-async function create(req, res) {
-  const data = await service.create(req.body.data);
-  res.status(201).json({ data });
+async function readTable( req, res, next) {
+  // console.log(req.params)
+  const data = await service.read(req.params.table_id)
+  if(data) {
+    res.locals.table = data
+    return next()
+  }
+  return next({ status: 404, message: `${req.params.table_id} not found`})
+}
+
+async function readRes(req, res, next) {
+  const data = await resService.read(req.body.data.reservation_id)
+  if(data){
+    res.locals.reservation = data
+    return next()
+  }
+  return next({ status: 404, message: `${req.body.data.reservation_id} not found`})
+}
+
+async function create(req,res) {
+  const data = await service.create(req.body.data)
+  res.status(201).json({ data })
+}
+
+
+async function update(req, res) {
+  const tableId = req.params.table_id
+  const tableData = req.body.data
+  const updatedTable = {
+    ...tableData,
+    table_id: tableId
+  }
+  const data = await service.update(updatedTable)
+  res.json({ data })
 }
 
 function bodyDataHas(propertyName) {
@@ -24,14 +56,13 @@ function bodyDataHas(propertyName) {
   };
 }
 
-function lengthOfTable_name(req,res,next){
-    const { data = {} } = req.body;
-    const table_name = data.table_name;
-    if(table_name.length > 1){
-        return next();
-
-    }
-    next({ status: 400, message: `table_name` });
+function isOneChar(req, res, next) {
+  const {data = {}} = req.body
+  const name = data.table_name;
+  if(name.length>1){
+    return next()
+  }
+  return next({ status: 400, message: 'table_name must be at least 2 characters'})
 }
 
 function isInteger(req,res,next) {
@@ -42,13 +73,42 @@ function isInteger(req,res,next) {
   return next({ status: 400, message: `capacity must be a number`})
 }
 
+
+function capacity(req,res,next) {
+  const reservation = res.locals.reservation.people
+  const table = res.locals.table.capacity
+  if( reservation > table ){
+    return next({ status : 400, message: `table does not have sufficient capacity`})
+  }
+  // console.log('test',data)
+  return next()
+}
+
+function occupied(req,res,next){
+  // console.log('data', req.body)
+  // console.log('info',res.locals.reservation, res.locals.table)
+  let occupied = res.locals.table.reservation_id
+  if(occupied){
+    return next( {status: 400, message: `table is occupied`})
+  }
+  return next()
+}
+
 module.exports = {
   list: [asyncErrorBoundary(list)],
   create: [
-    bodyDataHas("table_name"),
-    lengthOfTable_name,
+    asyncErrorBoundary(bodyDataHas("table_name")),
+    asyncErrorBoundary(isOneChar),
     bodyDataHas("capacity"),
-    isInteger,
+    asyncErrorBoundary(isInteger),
     asyncErrorBoundary(create)
-  ]
+  ],
+  // read: [readRes,read],
+  update: [
+    asyncErrorBoundary(bodyDataHas("reservation_id")),
+    asyncErrorBoundary(readRes),
+    asyncErrorBoundary(readTable),
+    asyncErrorBoundary(capacity),
+    asyncErrorBoundary(occupied),
+    asyncErrorBoundary(update)]
 }
--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -4,6 +4,10 @@ function list(){
     return knex("tables").select("*").orderBy("table_name");
 }
 
+function read(table_id){
+  return knex("tables").select("*").where({ "table_id": table_id}).first()
+}
+
 function create(newTable) {
     // Complete the implementation of this method.
   //   return newCustomer;
@@ -13,7 +17,17 @@ function create(newTable) {
     .then(tableData=>tableData[0])
   }
 
+
+  function update(table) {
+    return knex("tables")
+    .where({ table_id: table.table_id })
+    .update(table)
+    .returning("*")
+  }
+
 module.exports ={
     list,
-    create
+    read,
+    create,
+    update
 }
